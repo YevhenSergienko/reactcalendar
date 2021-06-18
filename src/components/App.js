517b0c612a9da7a1c6be9aa904cbc92e68c21ca6
Yevhen <zhekahome@gmail.com>
2021-06-18 16:24:11 +0300

    Add components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Year from './Year';
+import Month from './Month';
+import Day from './Day';
+
+function App() {
+  return (
+    <App>
+      <Year/>
+      <Month number={1} />
+      <Day number={18}/>
+    </App>
+  );
+}
+
+export default App;
---
+++ src/components/Year.jsx
@@ -0,0 +1,7 @@
+function Year() {
+    return (
+      
+    );
+  }
+  
+  export default Year;
